3f41d9c0de5c9849ec44370d0dd97366b9364abf
Rob MacEachern <rob@robmaceachern.com>
2011-10-23 23:38:44 -0700

    added the main method

--- src/ca/ubc/cpsc304/r3/Clerk.java
+++ src/ca/ubc/cpsc304/r3/Clerk.java
@@ -4,7 +4,12 @@ public class Clerk {
 	
 	public String fuckThePolice(){
 		String ftp = "FUCK THE POLICE!! http://www.youtube.com/watch?v=1M8vei3L0L8";
-		ftp = ftp + System.getProperty("line.separator") + " Clerks for life. Word";
+		ftp = ftp + System.getProperty("line.separator") + "Clerks for life. Word";
 		return ftp;
+	}
+	
+	public static void main(String[] args){
+		Clerk badAssClerk = new Clerk();
+		System.out.println(badAssClerk.fuckThePolice());
 	}
 }
