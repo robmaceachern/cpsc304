58e5fd4333a9a1ca405a0f5eb81fe5fdad582f8f
Rob MacEachern <rob@robmaceachern.com>
2011-11-03 12:56:13 -0700

    Created the base JDBC/tomcat project that we can work from.

---
+++ src/ca/ubc/cpsc304/r3/Main.java
@@ -0,0 +1,15 @@
+package ca.ubc.cpsc304.r3;
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		ClerkDao dao = new ClerkDao();
+		dao.readDataBase();
+
+	}
+
+}
